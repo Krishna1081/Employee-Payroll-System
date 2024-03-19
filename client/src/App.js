b146c6c7a940ae185ad1125b993185d409e7816a
Geswar003 <geswar003@gmail.com>
2024-03-19 16:11:12 +0530

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,28 @@
- import './App.css';
+import React from "react";
+import {  Route, Routes, Navigate  } from "react-router-dom";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/js/bootstrap.bundle";
 import Login from './app/login/login';
+import './App.css';
+// import Home from "./app/home/home";
+import Start from "./app/start_page/start";
+import HomePage from "./app/home/home_page";
 
-function App() {
+
+const App = () => {
   return (
-    <div>
-      <Login />
-    </div>
+    <>
+    {/* <AppBar/> */}
+      <Routes>
+        {/* <Route path="/" Component={Home}/> */}
+        <Route path="*" element={<Navigate replace to={"/"} />} />       
+        <Route path="/login" Component={Login} />
+        <Route path="/startpage" Component={Start} />        
+        <Route path="/homepage" Component={HomePage} />
+
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
